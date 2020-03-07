e8a7be147fec03647069acffe2ddb6830229d074
Kevin Tirta <kevintirta1@yahoo.com>
2020-03-07 15:28:01 +0200

    add assets

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,79 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/contact_us">Contact Us</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link to="/">Home</router-link>
+    <div id="main-bar">
+      <div id="nav">
+        <router-link to="/contact_us">Contact Us</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/">Home</router-link>
+      </div>
     </div>
     <router-view />
+    <div id="blocks">
+      <div id="frame_1" />
+      <div id="frame_2" />
+      <div id="frame_3" />
+      <div id="frame_4" />
+    </div>
   </div>
 </template>
 
 <style>
-body {
-  background: linear-gradient(270deg, #03001e, #7303c0,#ec38bc);
-  background-size: 400% 400%;
+#main-bar {
+  position: absolute;
+  width: 922px;
+  height: 1024px;
+  left: 0px;
+  top: 0px;
+  font-family: Avenir;
+  background: #000000;
+}
 
-  -webkit-animation: AnimationName 15s ease infinite;
-  -moz-animation: AnimationName 15s ease infinite;
-  animation: AnimationName 15s ease infinite;
+#frame_1 {
+  /* Frame 1 */
+
+  position: absolute;
+  width: 49px;
+  height: 143px;
+  left: 922px;
+  top: 1px;
+
+  background: #a9f7fb;
 }
-@-webkit-keyframes AnimationName {
-  0% {
-    background-position: 0% 50%;
-  }
-  50% {
-    background-position: 100% 50%;
-  }
-  100% {
-    background-position: 0% 50%;
-  }
+
+#frame_2 {
+  /* Rectangle 17 */
+
+  position: absolute;
+  width: 164px;
+  height: 202px;
+  left: 971px;
+  top: 0px;
+
+  background: #0529c8;
 }
-@-moz-keyframes AnimationName {
-  0% {
-    background-position: 0% 50%;
-  }
-  50% {
-    background-position: 100% 50%;
-  }
-  100% {
-    background-position: 0% 50%;
-  }
+
+#frame_3 {
+  /* Rectangle 18 */
+
+  position: absolute;
+  width: 72px;
+  height: 241px;
+  left: 1135px;
+  top: 1px;
+
+  background: #ffffff;
 }
-@keyframes AnimationName {
-  0% {
-    background-position: 0% 50%;
-  }
-  50% {
-    background-position: 100% 50%;
-  }
-  100% {
-    background-position: 0% 50%;
-  }
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #fff;
+
+#frame_4 {
+  /* Frame 2 */
+
+  position: absolute;
+  width: 234px;
+  height: 121px;
+  left: 1207px;
+  top: 0px;
+
+  background: #6e12e2;
 }
 
 #nav {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,64 +29,6 @@ export default {
 
 
 <style>
-:root {
-  --input-color: #99a3ba;
-  --input-border: #cdd9ed;
-  --input-background: #fff;
-  --input-placeholder: #cbd1dc;
 
-  --input-border-focus: #275efe;
 
-  --group-color: var(--input-color);
-  --group-border: var(--input-border);
-  --group-background: #eef4ff;
-
-  --group-color-focus: #fff;
-  --group-border-focus: var(--input-border-focus);
-  --group-background-focus: #678efe;
-}
-
-.form-group input {
-  width: 300px;
-  padding: 8px 16px;
-  line-height: 25px;
-  font-size: 14px;
-  font-weight: 500;
-  font-family: inherit;
-  border-radius: 6px;
-  -webkit-appearance: none;
-  color: var(--input-color);
-  border: 1px solid var(--input-border);
-  background: var(--input-background);
-  transition: border 0.3s ease;
-  margin-top: 25px;
-}
-
-span {
-        text-align: center;
-        padding: 10px 16px;
-        font-size: 14px;
-        line-height: 20px;
-        color: var(--group-color);
-        background: var(--group-background);
-        border: 1px solid var(--group-border);
-}
-
-#Company_logo {
-  /* font-family: ;*/
-  font-size: 25px;
-  color: aqua;
-  font-weight: bold;
-  display: block;
-}
-
-#Intro_slogan {
-  font-size: 40px;
-  font-weight: bold;
-}
-
-#moto_slogan {
-  font-size: 20px;
-  font-weight: 500;
-}
 </style>
