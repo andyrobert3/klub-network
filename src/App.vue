ff4bd39a21d93e000da6d9e3269cc31ae250ba97
Kevin Tirta <kevintirta1@yahoo.com>
2020-03-09 00:05:01 +0200

    MainPage FInish

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,47 @@
 </template>
 
 <style>
+
 @font-face {
   font-family: "Avenir";
   src: url("./assets/fonts/AvenirLTStd-Roman.otf");
 }
+
+@font-face {
+  font-family: "AvenirMedium";
+  src: url("./assets/fonts/AvenirLTStd-Medium.otf");
+}
+.BlueCircle{
+  background-color: #0500FF;
+  opacity: 20%;
+  border-radius: 50%;
+  position:absolute;
+  z-index: -1;
+}
+
+.RedCircle{
+  background-color: #ff008b;
+  opacity: 20%;
+  border-radius: 50%;
+  position:absolute;
+  z-index: -1;
+}
+
+.Small{
+  width: calc(100vw *(1/8));
+  height: calc(100vw *(1/8));
+}
+
+.Medium{
+  width: calc(100vw *(3/15));
+  height: calc(100vw *(3/15));
+}
+
+.Large{
+  width: calc(100vw *(3/12));
+  height: calc(100vw *(3/12));
+}
+
 body{
   font-family: "Avenir";
   margin:0px;
--- src/components/MainPage/MainPage.vue
+++ src/components/MainPage/MainPage.vue
@@ -1,7 +1,22 @@
 <template>
   <div>
     <div class="Left MainPage">
+      <div id="circle_1" class="BlueCircle Small"></div>
+      <div id="circle_2" class="RedCircle Medium"></div>
+      <div id="circle_3" class="BlueCircle Large"></div>
       <Navbar id="Navbar" v-bind:NavbarList="NavbarList"></Navbar>
+      <div id="MainPageSlogan">
+        <p id="Slogan_1">
+          Meet people, Build
+          <br />connections, and Repeat
+        </p>
+        <p id="Slogan_2">
+          The first indonesian networking platform to meet
+          <br />like-minded professionals
+        </p>
+        <button id="JoinUsButton">Join Us</button>
+        <p id="SignIn">Already registered? sign in</p>
+      </div>
     </div>
     <div class="Right MainPage">
       <div id="person_1"></div>
@@ -25,12 +40,59 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss">
+.MainPage {
+  position: relative;
+}
+
+#circle_1 {
+  right: calc(100vw * (1 / 16));
+  top: calc(100vw * (1 / 17));
+}
+
+#circle_2 {
+  left: calc(100vw * (1 / 16));
+  top: calc(100vw * (3 / 17));
+}
+
+#circle_3 {
+  right: calc(100vw * (1 / 16) * -1);
+  bottom: calc(100vw * (1 / 16) * -1);
+}
+
+#JoinUsButton {
+  width: 150px;
+  height: 41px;
+  font-size: 25px;
+  color: $WhiteColor;
+  background-color: $PurpleColor;
+  border-radius: 5px;
+  border: none;
+  font-weight: 600;
+}
+
+#Slogan_1 {
+  font-family: "AvenirMedium";
+  font-size: 36px;
+  margin: 0;
+  font-weight: bold;
+}
+
+#Slogan_2 {
+  font-size: 16px;
+}
+
+#MainPageSlogan {
+  margin-top: calc(100vh * (1 / 4));
+  text-align: center;
+}
+
 .Left {
   background-color: #000;
   height: 100%;
-  width: 100%;
+  width: calc(100vw - 375px);
   float: left;
+  z-index: -2;
 }
 
 .Right {
@@ -39,6 +101,15 @@ export default {
   width: 375px;
   right: 0px;
   position: absolute;
+}
+
+#MainPageSlogan {
+  color: $WhiteColor;
+  /* top:25%;
+  left:20%;
+  position: absolute; */
+  /* position: absolute; */
+  text-align: center;
 }
 
 #Navbar {
@@ -59,7 +130,7 @@ export default {
   /* height: 241px; */
   height: 33.33%;
   /* width: 214px; */
-  width:55%;
+  width: 55%;
   object-fit: cover;
   float: left;
   /* background-position-x: 300;   */
